c8761ebca11f995445f9727f41205cfb4b2d2a13
kesshinoda <odaketsu@gmail.com>
2025-07-15 22:59:51 -0500

    Added a part to send a car's info along with the picture

--- auto_trader/scraper.py
+++ auto_trader/scraper.py
@@ -9,191 +9,15 @@ from bs4 import BeautifulSoup
 import traceback
 import zendriver as ZenDriver
 
+from zendriver.core.connection import ProtocolException
+from zendriver.core.element import Element as ZenElement
+
+
 class UsedCarsScraper():
         
     def __init__(self):
         # Initialize something here
         return 
-    
-    def get_cars(
-    make="BMW", 
-    model="5 SERIES", 
-    postcode="SW1A 0AA", 
-    radius=1500, 
-    min_year=1995, 
-    max_year=1995, 
-    include_writeoff="include", 
-    max_attempts_per_page=5, 
-    verbose=False):
-
-        # To bypass Cloudflare protection
-        scraper = cloudscraper.create_scraper()
-
-        # Basic variables
-        results = []
-        n_this_year_results = 0
-
-        url = "https://www.autotrader.co.uk/results-car-search"
-
-        keywords = {}
-        keywords["mileage"] = ["miles"]
-        keywords["BHP"] = ["BHP"]
-        keywords["transmission"] = ["Automatic", "Manual"]
-        keywords["fuel"] = [
-        "Petrol", 
-        "Diesel", 
-        "Electric", 
-        "Hybrid – Diesel/Electric Plug-in", 
-        "Hybrid – Petrol/Electric", 
-        "Hybrid – Petrol/Electric Plug-in"
-        ]
-        keywords["owners"] = ["owners"]
-        keywords["body"] = [
-        "Coupe", 
-        "Convertible", 
-        "Estate", 
-        "Hatchback", 
-        "MPV", 
-        "Pickup", 
-        "SUV", 
-        "Saloon"
-        ]
-        keywords["ULEZ"] = ["ULEZ"]
-        keywords["year"] = [" reg)"]
-        keywords["engine"] = ["engine"]
-
-        # Set up parameters for query to autotrader.co.uk
-        params = {
-            "sort": "relevance",
-            "postcode": postcode,
-            "radius": radius,
-            "make": make,
-            "model": model,
-            "search-results-price-type": "total-price",
-            "search-results-year": "select-year",
-        }
-
-        if (include_writeoff == "include"):
-            params["writeoff-categories"] = "on"
-        elif (include_writeoff == "exclude"):
-            params["exclude-writeoff-categories"] = "on"
-        elif (include_writeoff == "writeoff-only"):
-            params["only-writeoff-categories"] = "on"
-            
-        year = min_year
-        page = 1
-        attempt = 1
-
-        try:
-            while year <= max_year:
-                params["year-from"] = year
-                params["year-to"] = year
-                params["page"] = page
-
-                r = scraper.get(url, params=params)
-                if verbose:
-                    print("Year:     ", year)
-                    print("Page:     ", page)
-                    print("Response: ", r)
-
-                try:
-                    if r.status_code != 200:   # If not successful (e.g. due to bot protection)
-                        attempt = attempt + 1  # Log as an attempt
-                        if attempt <= max_attempts_per_page:
-                            if verbose:
-                                print("Exception. Starting attempt #", attempt, "and keeping at page #", page)
-                        else:
-                            page = page + 1
-                            attempt = 1
-                            if verbose:
-                                print("Exception. All attempts exhausted for this page. Skipping to next page #", page)
-
-                    else:
-
-                        j = r.json()
-                        s = BeautifulSoup(j["html"], features="html.parser")
-
-                        articles = s.find_all("article", attrs={"data-standout-type":""})
-
-                        # If no results or reached end of results...
-                        if len(articles) == 0 or r.url[r.url.find("page=")+5:] != str(page):
-                            if verbose:
-                                print("Found total", n_this_year_results, "results for year", year, "across", page-1, "pages")
-                                if year+1 <= max_year:
-                                    print("Moving on to year", year + 1)
-                                    print("---------------------------------")
-
-                            # Increment year and reset relevant variables
-                            year = year + 1
-                            page = 1
-                            attempt = 1
-                            n_this_year_results = 0
-                        else:
-                            for article in articles:
-                                car = {}
-                                car["name"] = article.find("h3", {"class": "product-card-details__title"}).text.strip()             
-                                car["link"] = "https://www.autotrader.co.uk" + \
-                                    article.find("a", {"class": "listing-fpa-link"})["href"][: article.find("a", {"class": "listing-fpa-link"})["href"] \
-                                    .find("?")]
-                                car["price"] = article.find("div", {"class": "product-card-pricing__price"}).text.strip()
-
-                                seller_info = article.find("ul", {"class": "product-card-seller-info__specs"}).text.strip()
-                                car["seller"] = " ".join(seller_info.split())
-
-                                key_specs_bs_list = article.find("ul", {"class": "listing-key-specs"}).find_all("li")
-                                
-                                for key_spec_bs_li in key_specs_bs_list:
-
-                                    key_spec_bs = key_spec_bs_li.text
-
-                                    if any(keyword in key_spec_bs for keyword in keywords["mileage"]):
-                                        car["mileage"] = int(key_spec_bs[:key_spec_bs.find(" miles")].replace(",",""))
-                                    elif any(keyword in key_spec_bs for keyword in keywords["BHP"]):
-                                        car["BHP"] = int(key_spec_bs[:key_spec_bs.find("BHP")])
-                                    elif any(keyword in key_spec_bs for keyword in keywords["transmission"]):
-                                        car["transmission"] = key_spec_bs
-                                    elif any(keyword in key_spec_bs for keyword in keywords["fuel"]):
-                                        car["fuel"] = key_spec_bs
-                                    elif any(keyword in key_spec_bs for keyword in keywords["owners"]):
-                                        car["owners"] = int(key_spec_bs[:key_spec_bs.find(" owners")])
-                                    elif any(keyword in key_spec_bs for keyword in keywords["body"]):
-                                        car["body"] = key_spec_bs
-                                    elif any(keyword in key_spec_bs for keyword in keywords["ULEZ"]):
-                                        car["ULEZ"] = key_spec_bs
-                                    elif any(keyword in key_spec_bs for keyword in keywords["year"]):
-                                        car["year"] = key_spec_bs
-                                    elif key_spec_bs[1] == "." and key_spec_bs[3] == "L":
-                                        car["engine"] = key_spec_bs
-
-                                results.append(car)
-                                n_this_year_results = n_this_year_results + 1
-
-                            page = page + 1
-                            attempt = 1
-
-                            if verbose:
-                                print("Car count: ", len(results))
-                                print("---------------------------------")
-
-                except KeyboardInterrupt:
-                    break
-
-                except:
-                    traceback.print_exc()
-                    attempt = attempt + 1
-                    if attempt <= max_attempts_per_page:
-                        if verbose:
-                            print("Exception. Starting attempt #", attempt, "and keeping at page #", page)
-                    else:
-                        page = page + 1
-                        attempt = 1
-                        if verbose:
-                            print("Exception. All attempts exhausted for this page. Skipping to next page #", page)
-
-        except KeyboardInterrupt:
-            pass
-
-        return results
 
     async def add_trims_to_url(self, url:str, trim_codes_dicts:list) -> str:
         for model in trim_codes_dicts.keys():
@@ -216,18 +40,74 @@ class UsedCarsScraper():
         constructed_url = await self.add_trims_to_url(url, search_results_preference["trim_codes"])
         return constructed_url
     
-    async def get_car_list(self) -> list:
-        return []
-
-    async def main(self, search_results_preference:dict):
-        self._browser = await ZenDriver.start()
+    async def extract_car_info(self, car_info_tile:ZenElement) -> dict | None:
+        try:
+            title = (await self._page.query_selector("h2[data-cmp='subheading']", car_info_tile)).text
+            car_img_src = (await self._page.query_selector("img[data-cmp='inventoryImage']", car_info_tile))["src"]
+            car_mileage = (await self._page.query_selector("div[data-cmp='mileageSpecification']", car_info_tile)).text
+            car_price = (await self._page.query_selector("div[data-cmp='pricing']", car_info_tile)).text
+            owner_distance = (await self._page.query_selector("div[data-cmp='ownerDistance']", car_info_tile)).text
+            owner_phone_number = (await self._page.query_selector("span[data-cmp='phoneNumber']", car_info_tile)).text
+        except Exception as e:
+            print(f"Failed to extract car info: {e}")
+            return None
+        car_info_dict = {
+            "title": title,
+            "car_img_src": car_img_src,
+            "car_mileage": car_mileage,
+            "car_price": car_price, 
+            "owner_distance": owner_distance,
+            "owner_phone_number": owner_phone_number,
+        }
+        return car_info_dict
+    
+    async def get_car_list(self, list_length:int=3) -> list | None:
+        cars_info = []
+        try :
+            await self._page.wait_for_ready_state("complete")
+            await asyncio.sleep(0.5)
+            items_list = await self._page.query_selector_all("div[data-cmp='inventoryListing'] > div > div[data-cmp='itemCard']")
+        except (TimeoutError, ProtocolException):
+            print("Failed to load the results page")
+            return None
+        for item_index in range(1, list_length+1):
+            extracted_info = await self.extract_car_info(items_list[item_index])
+            if(extracted_info is None):
+                return None
+            cars_info.append(extracted_info)
+        return cars_info
+    
+    async def send_telegram_image_url(self, image_url:str, caption:str):
+        telegram_url = f"https://api.telegram.org/bot{os.getenv("TELEGRAM_TOKEN_1")}/sendPhoto"
+        payload = {
+            'chat_id': os.getenv("TELEGRAM_CHAT_ID"),
+            'photo': image_url,
+            'caption': caption,
+            'parse_mode': 'HTML'  # optional, for formatting
+        }
+        response = requests.post(telegram_url, data=payload)
+        return response.status_code == 200
+    
+    async def main(self, search_results_preference:dict, headless:bool):
+        self._browser = await ZenDriver.start(headless=headless)
         self._page = self._browser.main_tab
         await self._page.maximize()
         results_page_url = await self.construct_url(search_results_preference)
-        print(results_page_url)
         await self._page.get(results_page_url)
-
-        await asyncio.sleep(1000)
+        await asyncio.sleep(2)
+        results = await self.get_car_list()
+        if(results is None):
+            return print("An error occurred")
+        for car in results:
+            caption = (
+                f"🚗 <b>{car['title']}</b>\n"
+                f"📍 <b>Distance:</b> {car['owner_distance']}\n"
+                f"📞 <b>Phone:</b> {car['owner_phone_number']}\n"
+                f"💰 <b>Price:</b> {car['car_price']}\n"
+                f"🛣️ <b>Mileage:</b> {car['car_mileage']}"
+            )
+            if(await self.send_telegram_image_url(car['car_img_src'], caption) == False):
+                print("Failed to send a result!")
 
 if __name__ == "__main__":
     # car_info = {
@@ -250,4 +130,4 @@ if __name__ == "__main__":
     }
 
     class_instance = UsedCarsScraper()
-    asyncio.run(class_instance.main(search_results_preference))
+    asyncio.run(class_instance.main(search_results_preference, headless=True))
