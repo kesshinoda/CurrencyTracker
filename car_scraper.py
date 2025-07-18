8e20bc5a11a6090b58b14352958c7d27bb5d1c78
kesshinoda <odaketsu@gmail.com>
2025-07-17 21:10:28 -0500

    trying to switch to Selenium but not working

--- car_scraper.py
+++ car_scraper.py
@@ -3,26 +3,38 @@
 import asyncio
 import os
 import requests
+import selenium
+import time
 import zendriver as ZenDriver
 
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from zendriver.core.connection import ProtocolException
 from zendriver.core.element import Element as ZenElement
 
 
 class UsedCarsScraper():
         
-    def __init__(self):
-        # Initialize something here
+    def __init__(self, headless:bool):
+        chrome_options = Options()
+        if(headless):
+            chrome_options.add_argument("--headless")
+        self._driver = webdriver.Chrome(options=chrome_options)
         return 
 
-    async def add_trims_to_url(self, url:str, trim_codes_dicts:list) -> str:
+    def add_trims_to_url(self, url:str, trim_codes_dicts:list) -> str:
         for model in trim_codes_dicts.keys():
             for trim_code in trim_codes_dicts[model]:
                 trim_code = trim_code.replace(' ', '%20')
                 url+=f"trimCode={model.upper()}%7C{trim_code}&"
         return url[:len(url)-1] # remove the last '&'
 
-    async def construct_url(self, search_results_preference:dict) -> str:
+    def construct_url(self, search_results_preference:dict) -> str:
         url = "https://www.autotrader.com/cars-for-sale/"
         url += f"{search_results_preference['body_type']}/"
         url += f"{search_results_preference['make']}/"
@@ -33,17 +45,17 @@ class UsedCarsScraper():
         url += f"searchRadius={search_results_preference['search_radius']}&"
         url += f"sortBy={search_results_preference['sort_type']}&"
         url += f"startYear={search_results_preference['min_year']}&"
-        constructed_url = await self.add_trims_to_url(url, search_results_preference["trim_codes"])
+        constructed_url = self.add_trims_to_url(url, search_results_preference["trim_codes"])
         return constructed_url
     
-    async def extract_car_info(self, car_info_tile:ZenElement) -> dict | None:
+    def extract_car_info(self, car_info_tile:WebElement) -> dict | None:
         try:
-            title = (await self._page.query_selector("h2[data-cmp='subheading']", car_info_tile)).text
-            car_img_src = (await self._page.query_selector("img[data-cmp='inventoryImage']", car_info_tile))["src"]
-            car_mileage = (await self._page.query_selector("div[data-cmp='mileageSpecification']", car_info_tile)).text
-            car_price = (await self._page.query_selector("div[data-cmp='pricing']", car_info_tile)).text
-            owner_distance = (await self._page.query_selector("div[data-cmp='ownerDistance']", car_info_tile)).text
-            owner_phone_number = (await self._page.query_selector("span[data-cmp='phoneNumber']", car_info_tile)).text
+            title = (car_info_tile.find_element(By.XPATH, "//h2[@data-cmp='subheading']", )).text
+            car_img_src = (car_info_tile.find_element(By.XPATH, "//img[@data-cmp='inventoryImage']")).get_attribute("src")            
+            car_mileage = (car_info_tile.find_element(By.XPATH, "//div[@data-cmp='mileageSpecification']")).text
+            car_price = (car_info_tile.find_element(By.XPATH, "//div[@data-cmp='pricing']")).text
+            owner_distance = (car_info_tile.find_element(By.XPATH, "//div[@data-cmp='ownerDistance']")).text
+            owner_phone_number = (car_info_tile.find_element(By.XPATH, "//span[@data-cmp='phoneNumber']")).text
         except Exception as e:
             print(f"Failed to extract car info: {e}")
             return None
@@ -57,23 +69,21 @@ class UsedCarsScraper():
         }
         return car_info_dict
     
-    async def get_car_list(self, list_length:int=3) -> list | None:
+    def get_car_list(self, list_length:int=3) -> list | None:
         cars_info = []
         try :
-            await self._page.wait_for_ready_state("complete")
-            await asyncio.sleep(0.5)
-            items_list = await self._page.query_selector_all("div[data-cmp='inventoryListing'] > div > div[data-cmp='itemCard']")
-        except (TimeoutError, ProtocolException):
+            items_list = WebDriverWait(self._driver, 30).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-cmp='inventoryListing'] > div > div[data-cmp='itemCard']")))
+        except TimeoutException:
             print("Failed to load the results page")
             return None
         for item_index in range(1, list_length+1):
-            extracted_info = await self.extract_car_info(items_list[item_index])
+            extracted_info = self.extract_car_info(items_list[item_index])
             if(extracted_info is None):
                 return None
             cars_info.append(extracted_info)
         return cars_info
     
-    async def send_telegram_image_url(self, image_url:str, caption:str):
+    def send_telegram_image_url(self, image_url:str, caption:str):
         telegram_url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_TOKEN_1')}/sendPhoto"
         payload = {
             'chat_id': os.getenv("TELEGRAM_CHAT_ID"),
@@ -84,14 +94,12 @@ class UsedCarsScraper():
         response = requests.post(telegram_url, data=payload)
         return response.status_code == 200
     
-    async def main(self, search_results_preference:dict, headless:bool):
-        self._browser = await ZenDriver.start(headless=headless)
-        self._page = self._browser.main_tab
-        await self._page.maximize()
-        results_page_url = await self.construct_url(search_results_preference)
-        await self._page.get(results_page_url)
-        await asyncio.sleep(2)
-        results = await self.get_car_list()
+    def main(self, search_results_preference:dict):
+        
+        results_page_url = self.construct_url(search_results_preference)
+        self._driver.get(results_page_url)
+        time.sleep(10)
+        results = self.get_car_list()
         if(results is None):
             return print("An error occurred")
         for car in results:
@@ -102,15 +110,13 @@ class UsedCarsScraper():
                 f"💰 <b>Price:</b> {car['car_price']}\n"
                 f"🛣️ <b>Mileage:</b> {car['car_mileage']}"
             )
-            if(await self.send_telegram_image_url(car['car_img_src'], caption) == False):
+            if(self.send_telegram_image_url(car['car_img_src'], caption) == False):
                 print("Failed to send a result!")
+        
+    def end(self) -> None:
+        self._driver.quit()
 
 if __name__ == "__main__":
-    # car_info = {
-    #     "make": "honda",
-    #     "model": "civic",
-    #     "min_year": "2022",
-    # }
     search_results_preference = {
         "body_type": "hatchback",
         "make": "honda",
@@ -122,8 +128,7 @@ if __name__ == "__main__":
         "search_radius": "200",
         "sort_type": "derivedpriceASC", 
         "trim_codes": {"CIVIC": ["EX-L", "LX", "Sport", "Sport Touring"]},
-        # "zip_code": os.getenv("LOCAL_ZIP_CODE"),
     }
 
-    class_instance = UsedCarsScraper()
-    asyncio.run(class_instance.main(search_results_preference, headless=True))
+    class_instance = UsedCarsScraper(headless=False)
+    class_instance.main(search_results_preference)
